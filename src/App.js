29b967551b35b52704d29130f817b08956a2494a
1mlvn <96469615+1mlvn@users.noreply.github.com>
2022-04-25 19:24:27 +0200

    Making notes responsive with CSS grid

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
+import NotesList from './components/NotesList';
 import './index.css';
 
-function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+const App = () => {
+  return <div className='container'>
+    <NotesList />
+  </div>;
 }
 
 export default App;
--- src/components/Note.js
+++ src/components/Note.js
@@ -1,5 +1,14 @@
+import { BsTrashFill } from 'react-icons/bs';
+
+
 const Note = () => {
-    return <div className="note"></div>;
+    return <div className="note">
+        <span>sample text</span>
+        <div className="note-footer">
+            <small>25/04/2022</small>
+            <BsTrashFill className="delete-icon" size="1.25em"/>
+        </div>
+    </div>;
 };
 
 export default Note;
